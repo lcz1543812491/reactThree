17e55131d72921a68e95e2469bf39f8d50899e0b
lcz <1543812491@qq.com>
2023-09-08 15:27:50 +0800

    add lights

--- src/app/threeLight/utils.ts
+++ src/app/threeLight/utils.ts
@@ -9,18 +9,36 @@ import font1 from 'three/examples/fonts/helvetiker_regular.typeface.json'
 import { Inter, Roboto_Mono } from 'next/font/google'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
 
+
 export function inintThreeLight() {
+  const gui = new dat.GUI();
+
+  const common_material = new THREE.MeshStandardMaterial({color: 0xffffff, side: THREE.DoubleSide})
+  common_material.roughness = 0.3
+
+
   const scene = new THREE.Scene()
 
 
-  const ambentLight = new THREE.AmbientLight(0xfff, 0.1)
-  const pointLight = new THREE.PointLight(0xfff, 0.8)
-  pointLight.position.set(2, 3, 4)
-  scene.add(ambentLight)
-  scene.add(pointLight)
+  const ambentLight = new THREE.AmbientLight(0xffffff, 0.5)
+  gui.add(ambentLight, 'intensity').min(0).max(1).step(0.001)
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
-  camera.position.z = 3
+
+  const directLight = new THREE.DirectionalLight(0xffffff, 0.8)
+  directLight.position.set(2, 3, 4)
+
+  const light = new THREE.HemisphereLight( 0xff0000, 0x0000ff, 0.8 );
+
+  const pointLight = new THREE.PointLight( 0xff9000, 2, 1 ); 
+  pointLight.position.set( 1, 0.5, 1 ); 
+
+  scene.add(pointLight);
+  scene.add(light);
+  scene.add(ambentLight)
+  scene.add(directLight)
+
+  const camera = new THREE.PerspectiveCamera(90, (window as Window).innerWidth / (window as Window).innerHeight)
+  camera.position.z = 4
   scene.add(camera)
 
   const axisHelper = new THREE.AxesHelper()
@@ -33,6 +51,32 @@ export function inintThreeLight() {
 
   const controls = new OrbitControls(camera, render.domElement)
   controls.enableDamping = true
+
+
+  const geometry = new THREE.PlaneGeometry( 10, 10 );
+  const plane = new THREE.Mesh( geometry, common_material );
+  plane.rotation.x = Math.PI * 0.5
+  scene.add( plane );
+
+
+  const geometry1 = new THREE.SphereGeometry( 0.8, 32, 16 ); 
+  const sphere = new THREE.Mesh( geometry1, common_material );
+  sphere.position.y = 1
+  scene.add(sphere)
+
+  const geometry2 = new THREE.BoxGeometry( 1, 1, 1 ); 
+  const cube = new THREE.Mesh( geometry2, common_material ); 
+  cube.position.y =  1
+  cube.position.x = -2
+  scene.add( cube );
+
+
+  const geometry3 = new THREE.TorusGeometry( 0.5, 0.4, 16, 100 ); 
+  const torus = new THREE.Mesh( geometry3, common_material ); 
+  torus.position.y = 1
+  torus.position.x = 2
+  scene.add( torus );
+
 
   window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
