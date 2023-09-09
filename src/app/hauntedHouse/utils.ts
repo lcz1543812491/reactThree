f447012e73d6c78c7d27609f617cade7adb9e60d
lcz <1543812491@qq.com>
2023-09-09 18:38:51 +0800

    add lights

--- src/app/hauntedHouse/utils.ts
+++ src/app/hauntedHouse/utils.ts
@@ -12,38 +12,82 @@ import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
 export function inintHauntedHouse() {
   const scene = new THREE.Scene()
 
-
-  const ambentLight = new THREE.AmbientLight(0xfff, 1)
-  const pointLight = new THREE.PointLight(0xfff, 0.8)
-  pointLight.position.set(2, 3, 4)
+  const ambentLight = new THREE.AmbientLight('#b9d5ff', 0.5)
+  const directionalLight = new THREE.DirectionalLight('#b9d5ff', 2)
+  directionalLight.position.set(4, 5, -2)
 
   scene.add(ambentLight)
-  scene.add(pointLight)
+  scene.add(directionalLight)
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
-  camera.position.z = 3
+  camera.position.z = 20
+  camera.position.y = 10
+  camera.position.x = 10
   scene.add(camera)
 
   const axisHelper = new THREE.AxesHelper()
   scene.add(axisHelper)
 
-
-  const house  = new THREE.Group()
+  const house = new THREE.Group()
   scene.add(house)
 
+  const walls = new THREE.Mesh(new THREE.BoxGeometry(4, 2.5, 4), new THREE.MeshStandardMaterial({ color: '#ac8e82' }))
+  walls.position.y = 1.25
+  house.add(walls)
+
+  const roof = new THREE.Mesh(new THREE.ConeGeometry(3.5, 1, 4), new THREE.MeshStandardMaterial({color: '#b35f45'}))
+  roof.position.y = 3
+  roof.rotation.y = Math.PI * 0.25
+  house.add(roof)
+
+
+  const door = new THREE.Mesh(new THREE.BoxGeometry(2, 2), new THREE.MeshStandardMaterial({ color: '#aa7b7b' }))
+  door.position.y = 1
+  door.position.z = 1.501
+  house.add(door)
+
+  const bushmat = new THREE.MeshStandardMaterial({color: '#89c854'})
+  const bushge = new THREE.SphereGeometry(1, 16, 16)
+
+  const bush1 = new THREE.Mesh(bushge, bushmat)
+  bush1.scale.set(0.5, 0.5, 0.5)
+  bush1.position.set(0.8, 0.2, 2.2)
+
+  const bush2 = new THREE.Mesh(bushge, bushmat)
+  bush2.scale.set(0.25, 0.25, 0.25)
+  bush2.position.set(1.4, 0.1, 2.1)
+
+  house.add(bush1, bush2)
+
+
+  const graves = new THREE.Group()
+  scene.add(graves)
+
+  const grave_material = new THREE.MeshStandardMaterial({ color: '#b2b6b1' })
+  const grave_gem = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+
+  for(let i = 0; i < 50; i++){
+    const angle = Math.PI * Math.random() * 2
+    const radius = 3 + Math.random() * 6
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+    const grave = new THREE.Mesh(grave_gem, grave_material)
+    grave.position.set(x, 0, z)
+    grave.rotation.y = Math.random()
+    grave.rotation.z = (Math.random() - 0.5) * 0.6
+    graves.add(grave)
+  }
 
   const planeGeometry = new THREE.PlaneGeometry(20, 20)
   const planeMesh = new THREE.Mesh(planeGeometry, new THREE.MeshStandardMaterial({ color: '#a9c388' }))
-  planeMesh.rotation.x = - Math.PI * 0.5
+  planeMesh.rotation.x = -Math.PI * 0.5
   planeMesh.position.y = 0
-
 
   scene.add(planeMesh)
 
-  const render = new THREE.WebGL1Renderer({ antialias:true })
+  const render = new THREE.WebGL1Renderer({ antialias: true })
   render.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(render.domElement)
-
 
   const controls = new OrbitControls(camera, render.domElement)
   controls.enableDamping = true
