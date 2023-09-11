9894d027d7c7e23f29a36db487ac48d7b3bf06a3
lcz <1543812491@qq.com>
2023-09-11 19:33:24 +0800

    update

--- src/app/baseAnimate/utils.ts
+++ src/app/baseAnimate/utils.ts
@@ -4,28 +4,44 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 import { color } from 'three/examples/jsm/nodes/Nodes.js';
 
-//const gui = new dat.GUI();
-const parameter = {}
+
+const parameter = {
+  color: '#fe99ec'
+}
 
 
 
 export function inintBaseAnimate() {
 
-  const common_material = new THREE.MeshStandardMaterial({color: 0xffffff, side: THREE.DoubleSide})
-  common_material.roughness = 0.3
+  const common_material = new THREE.MeshStandardMaterial(
+    {
+      color: parameter.color,
+      metalness: 0.3,
+      roughness: 0.6
+    }
+  )
 
+//   const gui = new dat.GUI();
+
+//   gui.addColor(parameter, 'color').onChange((value) =>  {
+//     common_material.color.set(value)
+//   })
 
   const scene = new THREE.Scene()
 
+  const ambentLight = new THREE.AmbientLight(0xffffff, 0.5)
+  scene.add(ambentLight)
 
-  const ambentLight = new THREE.AmbientLight(0xffffff, 1)
+  const directLight = new THREE.DirectionalLight(0xffffff, 2)
+  directLight.position.set(4, 4, 4)
+  scene.add(directLight)
 
 
-  const directLight = new THREE.DirectionalLight(0xffffff, 0.8)
-  directLight.position.set(2, 3, 4)
+  const directionalLightCamera = new THREE.CameraHelper(directLight.shadow.camera)
+  // scene.add(directionalLightCamera)
 
 
-  const camera = new THREE.PerspectiveCamera(90, (window as Window).innerWidth / (window as Window).innerHeight)
+  const camera = new THREE.PerspectiveCamera(60, (window as Window).innerWidth / (window as Window).innerHeight)
   camera.position.z = 7
   scene.add(camera)
 
@@ -39,16 +55,27 @@ export function inintBaseAnimate() {
   })
   render.setSize(window.innerWidth, window.innerHeight)
 
-  const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-  scene.add(cube)
+  const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    common_material
+  )
+
+  const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 64, 128),
+    common_material
+  )
+
+  const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 200, 32),
+    common_material
+  )
+
+  scene.add(mesh1, mesh2, mesh3)
 
 
 
-  const controls = new OrbitControls(camera, render.domElement)
-  controls.enableDamping = true
+//   const controls = new OrbitControls(camera, render.domElement)
+//   controls.enableDamping = true
 
 
   window.addEventListener('resize', () => {
@@ -59,7 +86,7 @@ export function inintBaseAnimate() {
 
   function tick() {
     render.render(scene, camera)
-    controls.update()
+    //controls.update()
     requestAnimationFrame(tick)
   }
   tick()
