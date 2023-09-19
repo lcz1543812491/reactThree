313de2a51dc69cafb9930ae6737882d951e9fd42
lcz <1543812491@qq.com>
2023-09-19 16:20:48 +0800

    add model

--- src/app/realisticRender/utils.ts
+++ src/app/realisticRender/utils.ts
@@ -1,20 +1,17 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { mod } from 'three/examples/jsm/nodes/Nodes.js';
 
 
 export function realisticRender() {
-  const dat = require('dat.gui')
-  const gui = new dat.GUI();
 
   const scene = new THREE.Scene()
 
 
   const ambentLight = new THREE.AmbientLight(0xffffff, 0.2)
-  gui.add(ambentLight, 'intensity').min(0).max(1).step(0.001)
-
-
-  const directLight = new THREE.DirectionalLight(0xffffff, 0.4)
-  directLight.position.set(2, 3, 4)
+  const directLight = new THREE.DirectionalLight(0xffffff, 1)
+  directLight.position.set(0.25, 3, -2.25)
   directLight.castShadow = true
 
   directLight.shadow.mapSize.width = 1024
@@ -34,11 +31,28 @@ export function realisticRender() {
   directLightShadowHelper.visible = false
   scene.add(directLightShadowHelper);
 
+  const guiElements = document.getElementsByClassName('dg ac')
+  let gui: any;
+  if(guiElements.length === 0){
+    // console.log(guiElements)
+    const dat = require('dat.gui')
+    gui = new dat.GUI();
+    gui.add(directLight, 'intensity').min(0).max(10).step(0.001)
+    gui.add(directLight.position, 'x').min(-5).max(5).name('x')
+    gui.add(directLight.position, 'y').min(-5).max(5).name('y')
+    gui.add(directLight.position, 'z').min(-5).max(5).name('z')
+  }
 
-
-  scene.add(ambentLight)
+  //scene.add(ambentLight)
   scene.add(directLight)
 
+
+  const testSphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 32, 32), 
+    new THREE.MeshStandardMaterial()
+  )
+
+  scene.add(testSphere)
 
   const camera = new THREE.PerspectiveCamera(90, (window as Window).innerWidth / (window as Window).innerHeight)
   camera.position.z = 4
@@ -47,10 +61,12 @@ export function realisticRender() {
   const axisHelper = new THREE.AxesHelper()
   scene.add(axisHelper)
 
-  const render = new THREE.WebGL1Renderer({ antialias:true, canvas: document.getElementById('galaxy') as HTMLCanvasElement })
+  const render = new THREE.WebGLRenderer({ antialias:true, canvas: document.getElementById('galaxy') as HTMLCanvasElement })
   render.setSize(window.innerWidth, window.innerHeight)
   render.shadowMap.enabled = true
   render.shadowMap.type = THREE.PCFSoftShadowMap
+  // render.physicallyCorrectLights = true
+  // console.log('render', render.physicallyCorrectLights)
 
 
   const controls = new OrbitControls(camera, render.domElement)
@@ -59,6 +75,17 @@ export function realisticRender() {
 
   const clock = new THREE.Clock()
 
+  const gltfLoader = new GLTFLoader()
+
+  gltfLoader.load('/model/FlightHelmet/glTF/FlightHelmet.gltf', (model) => {
+    model.scene.scale.set(10, 10, 10)
+    model.scene.position.set(0, -4, 0)
+    scene.add(model.scene)
+    if(gui){
+        gui.add(model.scene.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.01)
+    }
+  })
+ 
 
   window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
