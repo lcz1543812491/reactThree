0d0e2c547bc6cf0ccdf5694b83bbc9fbbfb6055b
lcz <1543812491@qq.com>
2023-09-28 14:06:31 +0800

    add modifiedMaterials page

---
+++ src/app/modifiedMaterials/utils.ts
@@ -0,0 +1,114 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+function updateAllMaterials(scene: THREE.Scene) {
+    scene.traverse(child => {
+      if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+        child.material.envMapIntensity = 1
+        child.material.needsUpdate = true
+        child.castShadow = true
+        child.receiveShadow = true
+      }
+    })
+}
+
+export function realisticRender() {
+  const scene = new THREE.Scene()
+
+  const ambentLight = new THREE.AmbientLight(0xffffff, 0.2)
+  const directLight = new THREE.DirectionalLight(0xffffff, 1)
+  directLight.position.set(0.25, 3, -2.25)
+  directLight.castShadow = true
+
+  directLight.shadow.mapSize.width = 1024
+  directLight.shadow.mapSize.height = 1024
+
+  directLight.shadow.camera.top = 2
+  directLight.shadow.camera.bottom = -2
+  directLight.shadow.camera.left = -2
+  directLight.shadow.camera.right = 2
+
+  directLight.shadow.camera.near = 1
+  directLight.shadow.camera.far = 15
+
+  directLight.shadow.radius = 10
+
+  const directLightShadowHelper = new THREE.CameraHelper(directLight.shadow.camera)
+  directLightShadowHelper.visible = true
+  scene.add(directLightShadowHelper)
+
+  //scene.add(ambentLight)
+  scene.add(directLight)
+
+
+  const camera = new THREE.PerspectiveCamera(90, (window as Window).innerWidth / (window as Window).innerHeight)
+  camera.position.z = 4
+  scene.add(camera)
+
+  const axisHelper = new THREE.AxesHelper()
+  scene.add(axisHelper)
+
+  const render = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById('galaxy') as HTMLCanvasElement })
+  render.setSize(window.innerWidth, window.innerHeight)
+  render.shadowMap.enabled = true
+  render.shadowMap.type = THREE.PCFSoftShadowMap
+  render.outputColorSpace = THREE.SRGBColorSpace
+  render.toneMapping = THREE.ACESFilmicToneMapping
+  render.toneMappingExposure = 2
+  // render.physicallyCorrectLights = true
+  // console.log('render', render.physicallyCorrectLights)
+
+  const controls = new OrbitControls(camera, render.domElement)
+  controls.enableDamping = true
+
+  const clock = new THREE.Clock()
+
+  const gltfLoader = new GLTFLoader()
+
+  const cubeTextureLoader = new THREE.CubeTextureLoader()
+  const textureLoader = new THREE.TextureLoader()
+  const environmentMap = cubeTextureLoader.load(['/environmentMaps/0/px.jpg', '/environmentMaps/0/nx.jpg', '/environmentMaps/0/py.jpg', '/environmentMaps/0/ny.jpg', '/environmentMaps/0/pz.jpg', '/environmentMaps/0/nz.jpg'])
+
+  environmentMap.colorSpace = THREE.SRGBColorSpace
+  scene.background = environmentMap
+  scene.environment = environmentMap
+
+  
+  const mapTexture = textureLoader.load('/model/LeePerrySmith/color.jpg')
+  mapTexture.colorSpace = THREE. SRGBColorSpace 
+
+  const normalTexture = textureLoader.load('/model/LeePerrySmith/normal.jpg')
+
+  const material = new THREE.MeshStandardMaterial( {
+    map: mapTexture,
+    normalMap: normalTexture
+  })  
+
+  gltfLoader.load('/model/LeePerrySmith/LeePerrySmith.glb', model => {
+    const mesh = model.scene.children[0]
+    mesh.rotation.y = Math.PI * 0.5;
+
+    (mesh as any).material = material
+    scene.add(mesh)
+
+    // Update materials
+    updateAllMaterials(scene)
+  })
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    render.setSize(window.innerWidth, window.innerHeight)
+  })
+
+  function tick() {
+    const time = clock.getElapsedTime()
+
+    render.render(scene, camera)
+    controls.update()
+    requestAnimationFrame(tick)
+  }
+
+  tick()
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,53 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-black gap-8">
-      <Link href="/threedFont" className='text-white'>3D字体</Link>
-      <Link href="/threeLight" className='text-white'>Light 灯光</Link>
-      <Link href="/threeShadows" className='text-white'>Shadows 阴影</Link>
-      <Link href="/hauntedHouse" className='text-white'>haunted House</Link>
-      <Link href="/galaxy" className='text-white'>Galaxy</Link>
-      <Link href="/baseAnimate" className='text-white'>Base animate</Link>
-      <Link href="/physics" className='text-white'>Physics</Link>
-      <Link href="/foxModel" className='text-white'>Model</Link>
-      <Link href="/realisticRender" className='text-white'>Realistic Render</Link>
-      <Link href="/shader" className='text-white'>Shader</Link>
-      <Link href="/shaderDemo" className='text-white'>Shader Demo</Link>
-      <Link href="/shaderPattern1" className='text-white'>shader Pattern 1</Link>
-      <Link href="/ragingSea" className='text-white'>Raging Sea</Link>
+    <main className="flex flex-row">
+      <main className="flex min-h-screen flex-col items-center p-24 bg-black gap-8">
+        <Link href="/threedFont" className="text-white">
+          3D字体
+        </Link>
+        <Link href="/threeLight" className="text-white">
+          Light 灯光
+        </Link>
+        <Link href="/threeShadows" className="text-white">
+          Shadows 阴影
+        </Link>
+        <Link href="/hauntedHouse" className="text-white">
+          haunted House
+        </Link>
+        <Link href="/galaxy" className="text-white">
+          Galaxy
+        </Link>
+        <Link href="/baseAnimate" className="text-white">
+          Base animate
+        </Link>
+        <Link href="/physics" className="text-white">
+          Physics
+        </Link>
+        <Link href="/foxModel" className="text-white">
+          Model
+        </Link>
+        <Link href="/realisticRender" className="text-white">
+          Realistic Render
+        </Link>
+      </main>
+      <main className="flex min-h-screen flex-col items-center p-24 bg-black gap-8">
+        <Link href="/shader" className="text-white">
+          Shader
+        </Link>
+        <Link href="/shaderDemo" className="text-white">
+          Shader Demo
+        </Link>
+        <Link href="/shaderPattern1" className="text-white">
+          shader Pattern 1
+        </Link>
+        <Link href="/ragingSea" className="text-white">
+          Raging Sea
+        </Link>
+        <Link href="/modifiedMaterials" className="text-white">
+          Modified Materials
+        </Link>
+      </main>
     </main>
   )
 }
