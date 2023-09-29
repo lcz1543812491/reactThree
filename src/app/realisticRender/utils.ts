e526327496091e210d6e1a54d80d8fb258af633d
lcz <1543812491@qq.com>
2023-09-29 17:44:28 +0800

    add loading bar

--- src/app/realisticRender/utils.ts
+++ src/app/realisticRender/utils.ts
@@ -2,10 +2,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { mod } from 'three/examples/jsm/nodes/Nodes.js';
+import { gsap } from 'gsap'
 
 
 export function realisticRender() {
-
+  let gui: any;
   const scene = new THREE.Scene()
 
 
@@ -31,19 +32,9 @@ export function realisticRender() {
   directLightShadowHelper.visible = true
   scene.add(directLightShadowHelper);
 
-  const guiElements = document.getElementsByClassName('dg ac')
-  let gui: any;
-  if(guiElements.length === 0){
-    // console.log(guiElements)
-    const dat = require('dat.gui')
-    gui = new dat.GUI();
-    gui.add(directLight, 'intensity').min(0).max(10).step(0.001)
-    gui.add(directLight.position, 'x').min(-5).max(5).name('x')
-    gui.add(directLight.position, 'y').min(-5).max(5).name('y')
-    gui.add(directLight.position, 'z').min(-5).max(5).name('z')
-  }
 
-  //scene.add(ambentLight)
+
+  scene.add(ambentLight)
   scene.add(directLight)
 
 
@@ -52,14 +43,39 @@ export function realisticRender() {
     new THREE.MeshStandardMaterial()
   )
 
-  scene.add(testSphere)
+  // scene.add(testSphere)
 
   const camera = new THREE.PerspectiveCamera(90, (window as Window).innerWidth / (window as Window).innerHeight)
-  camera.position.z = 4
+  camera.position.z = 8
   scene.add(camera)
 
   const axisHelper = new THREE.AxesHelper()
   scene.add(axisHelper)
+
+
+  const planeGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+  const planeMaterial = new THREE.ShaderMaterial({
+    transparent: true,
+    uniforms: {
+      uAlpha: { value:1 }
+    },
+    vertexShader: `
+    void main()
+    {
+      gl_Position =  vec4(position, 1.0);
+    }
+    `,
+    fragmentShader: `
+    uniform float uAlpha;
+    void main()
+    {
+      gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+    }
+    `
+  })
+
+  const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+  scene.add(planeMesh)
 
   const render = new THREE.WebGLRenderer({ antialias:true, canvas: document.getElementById('galaxy') as HTMLCanvasElement })
   render.setSize(window.innerWidth, window.innerHeight)
@@ -78,9 +94,32 @@ export function realisticRender() {
 
   const clock = new THREE.Clock()
 
-  const gltfLoader = new GLTFLoader()
+  const loadingManager = new THREE.LoadingManager()
 
-  const cubeTextureLoader = new THREE.CubeTextureLoader()
+  const gltfLoader = new GLTFLoader(loadingManager)
+
+  const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
+
+  // loadingManager.onProgress = (value) => {
+  //   console.log('value', value)
+  // }
+
+  loadingManager.onLoad = () => {
+    console.log('onLoad')
+    gsap.to(planeMaterial.uniforms.uAlpha, { duration: 2, value: 0 })
+
+    const guiElements = document.getElementsByClassName('dg ac')
+    if(guiElements.length === 0){
+      // console.log(guiElements)
+      const dat = require('dat.gui')
+      gui = new dat.GUI();
+      gui.add(directLight, 'intensity').min(0).max(10).step(0.001)
+      gui.add(directLight.position, 'x').min(-5).max(5).name('x')
+      gui.add(directLight.position, 'y').min(-5).max(5).name('y')
+      gui.add(directLight.position, 'z').min(-5).max(5).name('z')
+    }
+  }
+
   const environmentMap = cubeTextureLoader.load([
     '/environmentMaps/0/px.jpg', 
     '/environmentMaps/0/nx.jpg', 
@@ -91,8 +130,8 @@ export function realisticRender() {
   ])
 
   environmentMap.colorSpace = THREE.SRGBColorSpace
-  scene.background = environmentMap
-  scene.environment = environmentMap
+  // scene.background = environmentMap
+  // scene.environment = environmentMap
  
 
   gltfLoader.load('/model/FlightHelmet/glTF/FlightHelmet.gltf', (model) => {
@@ -105,9 +144,9 @@ export function realisticRender() {
     scene.traverse((child) => {
         if((child as THREE.Mesh).material instanceof THREE.MeshStandardMaterial){
             // (child as any).material.envMap = environmentMap;
-            (child as any).material.envMapIntensity = 3
-            child.castShadow = true
-            child.receiveShadow = true
+            // (child as any).material.envMapIntensity = 3
+            // child.castShadow = true
+            // child.receiveShadow = true
             // console.log(child)
         }
     })
@@ -122,7 +161,7 @@ export function realisticRender() {
   })
 
   function tick() {
-    const time = clock.getElapsedTime()
+    // const time = clock.getElapsedTime()
     render.render(scene, camera)
     controls.update()
     requestAnimationFrame(tick)
