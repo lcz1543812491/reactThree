3ad85db97715492b03fce6adc092fedf4751503f
lcz <1543812491@qq.com>
2023-10-04 21:10:43 +0800

    add spector

--- src/app/portal/utils.ts
+++ src/app/portal/utils.ts
@@ -4,6 +4,12 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 export function initPortal() {
+
+//    const SPECTOR = require("spectorjs");
+
+//    const spector = new SPECTOR.Spector();
+//    spector.displayUI();
+
     /**
      * Base
      */
@@ -28,16 +34,45 @@ export function initPortal() {
     // GLTF loader
     const gltfLoader = new GLTFLoader()
     gltfLoader.setDRACOLoader(dracoLoader)
+
+    const bakedTexture = textureLoader.load('/model/portal/baked.jpg')
+    bakedTexture.flipY = false
+    bakedTexture.colorSpace = THREE.SRGBColorSpace
+
+ 
+    const baseMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
+    const lightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffe5 })
+
+    const lightMaterial1 = new THREE.MeshBasicMaterial({ color: 0xffffff })
+
+    gltfLoader.load('/model/portal/portal.glb', (model) => {
+    //   model.scene.traverse((child: any) => {
+    //     console.log(child)
+    //     child.material = baseMaterial
+    //   })
+
+      const baked: any = model.scene.children.find(item => item.name === 'baked')
+
+      baked.material = baseMaterial
+      const poleLightA: any = model.scene.children.find(item => item.name === 'poleLightA')
+      const poleLightB: any = model.scene.children.find(item => item.name === 'poleLightB')
+      const portalLight: any = model.scene.children.find(item => item.name === 'portalLight')
+      poleLightA.material = lightMaterial
+      poleLightB.material = lightMaterial
+      portalLight.material = lightMaterial1
+
+      scene.add(model.scene)
+    })
     
     /**
      * Object
      */
-    const cube = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial()
-    )
+    // const cube = new THREE.Mesh(
+    //     new THREE.BoxGeometry(1, 1, 1),
+    //     new THREE.MeshBasicMaterial()
+    // )
     
-    scene.add(cube)
+    // scene.add(cube)
     
     /**
      * Sizes
