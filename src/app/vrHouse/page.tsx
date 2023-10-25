f71713fcbde0af7519c2b56ec394a32dbf7aa41a
lcz <1543812491@qq.com>
2023-10-25 13:33:57 +0800

    add map

--- src/app/vrHouse/page.tsx
+++ src/app/vrHouse/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 import { vrHouse } from './utils'
+import Image from 'next/image'
 import { useEffect, useRef } from 'react'
 
 export default function Three() {
-
   const canvasRef = useRef(null as any)
 
   useEffect(() => {
     vrHouse(canvasRef.current as any)
   }, [])
 
-  return <canvas ref={canvasRef} id="galaxy" style={{ width: '100vw', height: '100vh', background: 'black' }}></canvas>
+  return (
+    <>
+      <div className="fixed bottom-0 left-0">
+        <Image className="absolute z-20" width="30" height="30" alt="location" src="/texture/vrHouse/location.png" />
+        <Image width="300" height="260" alt="map" src="/texture/vrHouse/map.gif" />
+      </div>
+      <canvas ref={canvasRef} id="galaxy" style={{ width: '100vw', height: '100vh', background: 'black' }}></canvas>
+    </>
+  )
 }
