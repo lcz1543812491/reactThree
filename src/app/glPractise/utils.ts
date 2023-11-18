fde86eac5c7158b4cabe0f7c801f2c89bc2a85d3
lcz <1543812491@qq.com>
2023-11-18 16:00:46 +0800

    delete extra code

--- src/app/glPractise/utils.ts
+++ src/app/glPractise/utils.ts
@@ -7,11 +7,7 @@ interface InintGlPractise {
 
 export function inintGlPractise(props: InintGlPractise) {
   const { canvasRef } = props
-
-  // console.log('canvasRef', canvasRef)
-
-  const canvasRefDom = document.getElementById('galaxy')
-  const webgl = (canvasRefDom as HTMLCanvasElement).getContext('webgl')
+  const webgl = canvasRef.getContext('webgl')
 
   // webgl?.viewport(0, 0, window.innerWidth, window.innerHeight)
   // webgl?.viewport(0, 0, canvasRefDom?.clientWidth || 0, canvasRefDom?.clientHeight || 0);
@@ -32,33 +28,6 @@ export function inintGlPractise(props: InintGlPractise) {
   const fragShader = `void main(){
     gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
   }`
-
-  // const vertShader1 = webgl?.createShader(webgl.VERTEX_SHADER) || ''
-  // const fragShader1 = webgl?.createShader(webgl.FRAGMENT_SHADER) || ''
-
-  // webgl?.shaderSource(vertShader1, vertShader)
-  // webgl?.shaderSource(fragShader1, fragShader)
-
-  // webgl?.compileShader(vertShader1)
-  // webgl?.compileShader(fragShader1)
-
-  // const program = webgl?.createProgram()
-
-  // webgl?.attachShader(program as WebGLProgram, vertShader1)
-  // webgl?.attachShader(program as WebGLProgram, fragShader1)
-
-  // webgl?.linkProgram(program as WebGLProgram)
-  // webgl?.useProgram(program as WebGLProgram)
-
-  //   const positions = new Float32Array([0.3, 0.5, 0.1, 1.0, 0.0, 1.0])
-
-  //   const aPosition = webgl?.getAttribLocation(program as WebGLProgram, 'a_position') || 0
-
-  //   webgl?.vertexAttrib4fv(aPosition, positions)
-
-  //   const proj1 = webgl?.getUniformLocation(program as WebGLProgram, 'proj')
-
-  //   webgl?.uniformMatrix4fv(proj1 as  WebGLUniformLocation, false, projectMatrix)
 
   initShaders({ gl: webgl as WebGLRenderingContext, fragmentSource: fragShader, vertexSource: vertShader })
 
