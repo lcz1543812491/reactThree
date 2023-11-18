0a782f6e3dd6a966d79baf54addc0c5e9e7bcb37
lcz <1543812491@qq.com>
2023-11-18 20:55:24 +0800

    draw points

--- src/app/glPractise/utils.ts
+++ src/app/glPractise/utils.ts
@@ -18,26 +18,40 @@ export function inintGlPractise(props: InintGlPractise) {
 
   const vertShader = `
   attribute vec2 a_position;
+  attribute float a_size;
   void main(){
       gl_Position = vec4(a_position, 0.0, 1.0);
-      gl_PointSize = 40.0;
+      gl_PointSize = a_size;
   }
   `
 
   const fragShader = `void main(){
-    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
   }`
 
   const program = initShaders({ gl: webgl as WebGLRenderingContext, fragmentSource: fragShader, vertexSource: vertShader })
 
-  const attrLocation = webgl?.getAttribLocation(program as WebGLProgram, 'a_position')
+
 
   // webgl?.vertexAttrib2f(attrLocation as number, -0.8, 0.0)
-
-  webgl?.vertexAttrib2fv(attrLocation as number, new Float32Array([-0.8, 0.1]))
-
-
   webgl?.clearColor(0.0, 0.0, 0.0, 1.0)
   webgl?.clear(webgl.COLOR_BUFFER_BIT)
-  webgl?.drawArrays(webgl.POINTS, 0, 1)
+
+  let x = 0, y = 0;
+
+  for(let i = 0; i < 1000; i++){
+    const r = i/100
+    x = r * Math.cos(i)
+    y = r * Math.sin(i)
+
+    const a_position = webgl?.getAttribLocation(program as WebGLProgram, 'a_position')
+    webgl?.vertexAttrib2f(a_position as number, x, y)
+
+    const a_size = webgl?.getAttribLocation(program as WebGLProgram, 'a_size')
+    webgl?.vertexAttrib1f(a_size as number, r * 10)
+
+    webgl?.drawArrays(webgl.POINTS, 0, 1)
+    // webgl?.vertexAttrib2fv(attrLocation as number, new Float32Array([x, y]))
+  }
+
 }
